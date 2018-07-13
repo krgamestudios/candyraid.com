2ac96bee803e64c20c3529a5da4717404af4621d
Kayne <kayneruse@gmail.com>
2018-07-13 12:17:37 +1000

    Added link previews

--- public_html/src/pages/page_home.jsx
+++ public_html/src/pages/page_home.jsx
@@ -17,6 +17,7 @@ class PageHome extends React.Component {
           <div style={{paddingLeft: '5px'}}>
             <p>Milly Monka's Candy Factory is the biggest producer of candy in the whole world. So naturally, she set up traps and defenses to protect her factory from thieves. One such thief is Candy the witch - a powerful witch with a powerful sweet tooth.</p>
             <p>Candy must make her way through each section of the factory - the gumdrop gardens, the lava kitchens and the frozen vaults - to steal as much candy as possible!</p>
+            <p>This is a casual top-down puzzle game, suitable for all ages.</p>
             <p>We're aiming for an October release, but you can play the demo now!</p>
             <ul>
               <li><a href="https://krgamestudios.com/candyraid">Candy Raid: The Factory Demo</a></li>
