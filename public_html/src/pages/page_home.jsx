2b2a838dd4e76c319278401be0b67bfbd419e600
Kayne <kayneruse@gmail.com>
2018-07-17 20:58:29 +1000

    Added store page link

--- public_html/src/pages/page_home.jsx
+++ public_html/src/pages/page_home.jsx
@@ -15,11 +15,12 @@ class PageHome extends React.Component {
         <div className="responsive">
           <img src="/img/crtfpromo.png" className="responsive" />
           <div style={{paddingLeft: '5px'}}>
-            <p>Milly Monka's Candy Factory is the biggest producer of candy in the whole world. So naturally, she set up traps and defenses to protect her factory from thieves. One such thief is Candy the witch - a powerful witch with a powerful sweet tooth.</p>
+            <p>Milly Monk's Candy Factory is the biggest producer of candy in the whole world. So naturally, she set up traps and defenses to protect her factory from thieves. One such thief is Candy the witch - a powerful witch with a powerful sweet tooth.</p>
             <p>Candy must make her way through each section of the factory - the gumdrop gardens, the lava kitchens and the frozen vaults - to steal as much candy as possible!</p>
             <p>This is a casual top-down puzzle game, suitable for all ages.</p>
             <p>We're aiming for an October release, but you can play the demo now!</p>
             <ul>
+              <li><a href="https://store.steampowered.com/app/868880/Candy_Raid_The_Factory/">Candy Raid: The Factory Store Page</a></li>
               <li><a href="https://krgamestudios.com/candyraid">Candy Raid: The Factory Demo</a></li>
               <li><a href="https://krgamestudios.com/candyraid/presskit">Press Kit</a></li>
             </ul>
