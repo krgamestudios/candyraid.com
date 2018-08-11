a3c6eebc1dc8908dff4d51b709dd53cc083c612e
Kayne <kayneruse@gmail.com>
2018-08-12 09:46:40 +1000

    Fixed grammar

--- public_html/src/pages/page_prototype.jsx
+++ public_html/src/pages/page_prototype.jsx
@@ -19,7 +19,7 @@ class PagePrototype extends React.Component {
     return (
       <div style={flexStyle}>
         <center>
-          <p><strong>WARNING:</strong> This is a Candy Raid's PROTOTYPE. It does not reflect the current state of the game.</p>
+          <p><strong>WARNING:</strong> This is Candy Raid: The Factory's <strong>PROTOTYPE</strong>. It does not reflect the current state of the game.</p>
         </center>
         <PanelUnity name='prototype' width='960px' height='600px' />
       </div>
